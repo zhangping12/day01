560dd3e7dd22cf4f932fc5bccdbb15d27ba2cbe6
zp <634001184@qq.com>
2018-10-25 12:17:44 +0800

    update

--- src/cn/itcast/genecity/Demo3.java
+++ src/cn/itcast/genecity/Demo3.java
@@ -1,5 +1,12 @@
 package cn.itcast.genecity;
 
+import javafx.concurrent.Worker;
+
+import java.util.Comparator;
+
+import java.util.ArrayList;
+
+
 /*
 泛型类
 
@@ -12,6 +19,13 @@ package cn.itcast.genecity;
      1、在类上自定义的泛型的具体数据类型是在创建对象的时候指定的
      2、在类上自定义的泛型，如果创建该类的对象时没有指定泛型的具体类型，那么默认是Object类型。
  */
+class worker implements Comparator<Worker>{
+    @Override
+    public int compare(Worker o1, Worker o2) {
+        return 0;
+    }
+}
+
 
 //自定义 一个集合对象
 class MyList<T>{
@@ -34,5 +48,7 @@ public class Demo3 {
         list.add("123");
 
         MyList list2 = new MyList<>();//不指定，add则是object类型
+
+        new ArrayList<String>();//如果指定了为String，那么后面的调用的方法都是String
     }
 }
